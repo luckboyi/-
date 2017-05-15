29a370a54a67e04ad188f1273fa38502e5212d4f
546217953@qq.com <546217953@qq.com>
2017-05-15 10:27:10 +0800

    demo

---
+++ 、、/手机新浪网_files/config.js
@@ -0,0 +1,9 @@
+addjs.setConfig({
+  debugMap:{
+	'http://mjs.sinaimg.cn/wap/online/home/v7/css/home.min.css':'./css/home.min.css',
+    'http://mjs.sinaimg.cn/wap/online/home/v7/js/homev7.min.js':'./js/homev7.js',
+    'http://wap_front.dev.sina.cn/yuanyuan/homev7/js/homev7.min.js':'./js/homev7.js'
+  },
+  version:'0.0.110'
+
+});
